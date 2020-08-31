80cca348f4ed0759efbbd005d916dd64db5231ed
ts-miller <tyler@DESKTOP-RNOETJO.localdomain>
2020-08-31 14:31:36 -0400

    Done.

---
+++ lib/doctor.rb
@@ -0,0 +1,28 @@
+class Doctor
+    attr_accessor :name
+
+    @@all = []
+
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def appointments
+        Appointment.all.select do |appointment|
+            appointment.doctor == self
+        end
+    end
+
+    def new_appointment(date, patient)
+        Appointment.new(date, patient, self)
+    end
+
+    def patients
+        appointments.collect(&:patient)
+    end
+end
---
+++ lib/genre.rb
@@ -0,0 +1,24 @@
+class Genre
+    attr_accessor :name
+
+    @@all = []
+   
+    def initialize(name)
+        @name = name
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end
+
+    def songs
+        Song.all.select do |song|
+            song.genre == self
+        end
+    end
+
+    def artists
+        songs.collect(&:artist)
+    end
+end
